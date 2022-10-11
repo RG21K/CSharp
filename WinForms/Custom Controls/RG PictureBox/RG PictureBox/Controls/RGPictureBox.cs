f0909f1e6c42efa7cb885435e13c6ea63d66565a
Ricardo Garcia <102972020+RG21K@users.noreply.github.com>
2022-10-11 15:40:35 +0100

    Add files via upload

---
+++ WinForms/Custom Controls/RG PictureBox/RG PictureBox/Controls/RGPictureBox.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+// <copyright file="RGPictureBox.cs" company="None"> Copyright (c) Ricardo Garcia. </copyright>
+// <author> Ricardo Garcia </author>
+// <date> 2022, 10.10 </date>
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RG_PictureBox.Controls
+{
+    public class RGPictureBox : PictureBox
+    {
+        // Last Edited: 2022, 10.10
+        #region <Configuration>
+        /// <summary> Set Control Style Configuration. </summary>
+        private void SetStyles()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+        #endregion
+
+
+        #region <Constructor>
+        public RGPictureBox()
+        {
+            SetStyles();
+
+            this.Size = new Size(150, 150);
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        #endregion
+
+
+        #region <Custom Properties>
+        private int borderSize = 2;
+        [Category("1. Custom Properties")]
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        private Color borderColor = Color.Red;
+        [Category("1. Custom Properties")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+        private Color borderColor2 = Color.Red;
+        [Category("1. Custom Properties")]
+        public Color BorderColor2
+        {
+            get { return borderColor2; }
+            set
+            {
+                borderColor2 = value;
+                this.Invalidate();
+            }
+        }
+
+        private DashStyle borderLineStyle = DashStyle.Solid;
+        [Category("1. Custom Properties")]
+        public DashStyle BorderLineStyle
+        {
+            get { return borderLineStyle; }
+            set
+            {
+                borderLineStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        private DashCap borderCapStyle = DashCap.Flat;
+        [Category("1. Custom Properties")]
+        public DashCap BorderCapStyle
+        {
+            get { return borderCapStyle; }
+            set
+            {
+                borderCapStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        private float gradientAngle = 50F;
+        [Category("1. Custom Properties")]
+        public float GradientAngle
+        {
+            get { return gradientAngle; }
+            set
+            {
+                gradientAngle = value;
+                this.Invalidate();
+            }
+        }
+        #endregion
+
+
+        #region <Overriden Events>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            this.Size = new Size(this.Width, this.Width);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // -> Rendering Configuration (Set Here and Pass to Further Drawing Methods via PaintEventArgs Parameter)
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RedrawControl(e);
+        }
+        #endregion
+
+
+        #region <Methods> : (Drawing)
+        private void RedrawControl(PaintEventArgs e)
+        {
+            var graph = e.Graphics;
+            var rectContourSmooth = Rectangle.Inflate(this.ClientRectangle, -1, -1);
+            var rectBorder = Rectangle.Inflate(rectContourSmooth, -borderSize, -borderSize);
+            var smoothSize = borderSize > 0 ? borderSize * 3 : 1;
+            
+            using (var borderGColor = new LinearGradientBrush(rectBorder, borderColor, borderColor2, gradientAngle))
+            using (var pathRegion = new GraphicsPath())
+            using (var penSmooth = new Pen(this.Parent.BackColor, smoothSize))
+            using (var penBorder = new Pen(borderGColor, borderSize))
+            {
+                penBorder.DashStyle = borderLineStyle;
+                penBorder.DashCap = borderCapStyle;
+                pathRegion.AddEllipse(rectContourSmooth);
+
+                // -> Set Rounded Region 
+                this.Region = new Region(pathRegion);
+
+                // -> Draw Contour Smoothing
+                graph.DrawEllipse(penSmooth, rectContourSmooth);
+
+                // -> Draw Border
+                if (borderSize > 0) { graph.DrawEllipse(penBorder, rectBorder); }
+            }
+        }
+        #endregion
+    }
+}
---
+++ WinForms/Custom Controls/RG PictureBox/RG PictureBox/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿// <copyright file="Form1.Designer.cs" company="None"> Copyright (c) Ricardo Garcia. </copyright>
+// <author> Ricardo Garcia </author>
+// <date> 2022, 10.10 </date>
+namespace RG_PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rgPictureBox1 = new RG_PictureBox.Controls.RGPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.rgPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rgPictureBox1
+            // 
+            this.rgPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.rgPictureBox1.BorderColor = System.Drawing.Color.White;
+            this.rgPictureBox1.BorderColor2 = System.Drawing.Color.DeepSkyBlue;
+            this.rgPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.rgPictureBox1.BorderSize = 2;
+            this.rgPictureBox1.GradientAngle = 50F;
+            this.rgPictureBox1.Location = new System.Drawing.Point(25, 24);
+            this.rgPictureBox1.Name = "rgPictureBox1";
+            this.rgPictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.rgPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rgPictureBox1.TabIndex = 0;
+            this.rgPictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(200, 200);
+            this.Controls.Add(this.rgPictureBox1);
+            this.ForeColor = System.Drawing.Color.Gainsboro;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.rgPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.RGPictureBox rgPictureBox1;
+    }
+}
+
