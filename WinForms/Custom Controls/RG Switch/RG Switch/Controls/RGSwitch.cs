44f633c3329e72370e713cdfbdf418a8dc8ed348
Ricardo Garcia <102972020+RG21K@users.noreply.github.com>
2022-10-11 16:03:05 +0100

    Add files via upload

---
+++ WinForms/Custom Controls/RG Switch/RG Switch/Controls/RGSwitch.cs
@@ -0,0 +1,198 @@
+﻿// <copyright file="RGSwitch.cs" company="None"> Copyright (c) Ricardo Garcia. </copyright>
+// <author> Ricardo Garcia </author>
+// <date> 2022, 10.10 </date>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RG_Switch.Controls
+{
+    public class RGSwitch : CheckBox
+    {
+        // Last Edited: 2022, 10.10
+
+        #region <Developer Notes>
+        /*
+         * [Ideas to Implement]
+         * 
+         *          [1]. Toggle Orientation (Horizontal & Vertical)
+         *          [2]. When Solid Design is Selected: Set also the Background Colors (Checked / Unchecked).
+         * 
+         */
+        #endregion
+
+        #region <Constructor>
+        public RGSwitch()
+        {
+            // Requests the Form to use a Secondary Buffer to Redraw the Content.
+            // You can Set Either: DoubleBuffered = true;
+            // or
+            // SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            //
+            // Link: https://docs.microsoft.com/en-us/dotnet/desktop/winforms/advanced/how-to-reduce-graphics-flicker-with-double-buffering-for-forms-and-controls?view=netframeworkdesktop-4.8
+            //
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            MinimumSize = new Size(40, 20);
+            Size = new Size(40, 20);
+        }
+        #endregion
+
+
+        #region <Fields>
+        private bool defAutoSize = false;
+        #endregion
+
+
+        #region <Properties> : (Overriden)
+        public override bool AutoSize
+        {
+            get { return base.AutoSize; }
+            set { base.AutoSize = defAutoSize; }
+        }
+
+        public override string Text
+        {
+            get { return base.Text; }
+        }
+        #endregion
+
+
+        #region <Custom Properties>
+        private bool solidStyle = false;
+        [Category("1. Style"), DisplayName("0. IsSolidStyle")]
+        [Description("Set the Control Design Style.")]
+        [Bindable(true)]        // Required for Enum Types.
+        [Browsable(true)]
+        [DefaultValue(true)]    // What the Default Property Value is (It Does NOT Set the Actual Default Value).
+        public bool IsSolidStyle
+        {
+            get { return solidStyle; }
+            set { solidStyle = value; Invalidate(); }
+        }
+
+        private Color enabledBackColor = Color.Silver;
+        [Category("1. Style"), DisplayName("1. Enabled BackColor")]
+        [Description("Set Back Color for Enabled Switch Condition.")]
+        [Bindable(true)]        // Required for Enum Types.
+        [Browsable(true)]
+        public Color EnabledBackColor
+        {
+            get { return enabledBackColor; }
+            set { enabledBackColor = value; Invalidate(); }
+        }
+
+        private Color disabledBackColor = Color.Silver;
+        [Category("1. Style"), DisplayName("2. Disabled BackColor")]
+        [Description("Set Back Color for Disabled Switch Condition.")]
+        [Bindable(true)]        // Required for Enum Types.
+        [Browsable(true)]
+        public Color DisabledBackColor
+        {
+            get { return disabledBackColor; }
+            set { disabledBackColor = value; Invalidate(); }
+        }
+
+        private Color enabledKnobColor = Color.FromArgb(255, 38, 224, 127);
+        [Category("1. Style"), DisplayName("3. Enabled KnobColor")]
+        [Description("Set Knob Color for Enabled Switch Condition.")]
+        [Bindable(true)]        // Required for Enum Types.
+        [Browsable(true)]
+        public Color EnabledKnobColor
+        {
+            get { return enabledKnobColor; }
+            set { enabledKnobColor = value; Invalidate(); }
+        }
+
+        private Color disabledKnobColor = Color.Crimson;
+        [Category("1. Style"), DisplayName("4. Disabled KnobColor")]
+        [Description("Set Knob Color for Enabled Switch Condition.")]
+        [Bindable(true)]        // Required for Enum Types.
+        [Browsable(true)]
+        public Color DisabledKnobColor
+        {
+            get { return disabledKnobColor; }
+            set { disabledKnobColor = value; Invalidate(); }
+        }
+        #endregion
+
+
+        #region <Overriden Events>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // -> Configuration for Best Smooth Graphics
+            // Ref: https://stackoverflow.com/questions/33878184/c-sharp-how-to-make-smooth-arc-region-using-graphics-path
+            e.Graphics.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            e.Graphics.CompositingQuality = CompositingQuality.HighQuality;
+            e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality; // May Cause Glitches
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            e.Graphics.Clear(Parent.BackColor);
+
+            RedrawControl(e);
+        }
+        #endregion
+
+
+        #region <Methods> : (Drawing)
+        private GraphicsPath GetFigurePath()
+        {
+            // Default Block:
+            //int arcSize = Height - 1;
+            //Rectangle leftArc = new Rectangle(0, 0, arcSize, arcSize);
+            //Rectangle rightArc = new Rectangle(Width - arcSize - 2, 0, arcSize, arcSize);
+
+            // Corrected Block (! Miss Margin !)
+            int arcSize = Height - 2;
+            Rectangle leftArc = new Rectangle(1, 1, arcSize, arcSize);
+            Rectangle rightArc = new Rectangle(Width - arcSize - 2, 1, arcSize, arcSize);
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(leftArc, 90, 180);
+            path.AddArc(rightArc, 270, 180);
+            path.CloseFigure();
+
+            return path;
+        }
+        private void RedrawControl(PaintEventArgs pe)
+        {
+            // int toggleSize = Height - 5; // DEFAULT LINE
+            int toggleSize = Height - 6;
+
+            if (Checked)
+            {
+                // Draw the Surface
+                if (IsSolidStyle) { pe.Graphics.FillPath(new SolidBrush(enabledBackColor), GetFigurePath()); }
+                else { pe.Graphics.DrawPath(new Pen(enabledBackColor, 2), GetFigurePath()); }
+
+                // Draw the Toggle Marker
+                //pe.Graphics.FillEllipse(new SolidBrush(enabledKnobColor), new Rectangle(Width - Height + 1, 2, toggleSize, toggleSize)); // DEFAULT LINE
+                pe.Graphics.FillEllipse(new SolidBrush(enabledKnobColor), new Rectangle(Width - Height + 1, 3, toggleSize, toggleSize));
+                this.Checked = true;
+            }
+
+            else
+            {
+                // Draw the Surface
+                if (solidStyle) { pe.Graphics.FillPath(new SolidBrush(disabledBackColor), GetFigurePath()); }
+                else { pe.Graphics.DrawPath(new Pen(disabledBackColor, 2), GetFigurePath()); }
+
+                // Draw the Toggle Marker
+                // pe.Graphics.FillEllipse(new SolidBrush(disabledKnobColor), new Rectangle(2, 2, toggleSize, toggleSize)); // DEFAULT LINE
+                pe.Graphics.FillEllipse(new SolidBrush(disabledKnobColor), new Rectangle(4, 3, toggleSize, toggleSize));
+                this.Checked = false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ WinForms/Custom Controls/RG Switch/RG Switch/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿// <copyright file="Form1.Designer.cs" company="None"> Copyright (c) Ricardo Garcia. </copyright>
+// <author> Ricardo Garcia </author>
+// <date> 2022, 10.10 </date>
+
+namespace RG_Switch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rgSwitch1 = new RG_Switch.Controls.RGSwitch();
+            this.rgSwitch2 = new RG_Switch.Controls.RGSwitch();
+            this.rgSwitch3 = new RG_Switch.Controls.RGSwitch();
+            this.rgSwitch4 = new RG_Switch.Controls.RGSwitch();
+            this.rgSwitch5 = new RG_Switch.Controls.RGSwitch();
+            this.rgSwitch6 = new RG_Switch.Controls.RGSwitch();
+            this.rgSwitch7 = new RG_Switch.Controls.RGSwitch();
+            this.rgSwitch8 = new RG_Switch.Controls.RGSwitch();
+            this.SuspendLayout();
+            // 
+            // rgSwitch1
+            // 
+            this.rgSwitch1.DisabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch1.DisabledKnobColor = System.Drawing.Color.Crimson;
+            this.rgSwitch1.EnabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch1.EnabledKnobColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(224)))), ((int)(((byte)(127)))));
+            this.rgSwitch1.IsSolidStyle = false;
+            this.rgSwitch1.Location = new System.Drawing.Point(12, 42);
+            this.rgSwitch1.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch1.Name = "rgSwitch1";
+            this.rgSwitch1.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch1.TabIndex = 0;
+            this.rgSwitch1.Text = "rgSwitch1";
+            this.rgSwitch1.UseVisualStyleBackColor = true;
+            // 
+            // rgSwitch2
+            // 
+            this.rgSwitch2.DisabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch2.DisabledKnobColor = System.Drawing.Color.Crimson;
+            this.rgSwitch2.EnabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch2.EnabledKnobColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(224)))), ((int)(((byte)(127)))));
+            this.rgSwitch2.IsSolidStyle = false;
+            this.rgSwitch2.Location = new System.Drawing.Point(12, 68);
+            this.rgSwitch2.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch2.Name = "rgSwitch2";
+            this.rgSwitch2.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch2.TabIndex = 1;
+            this.rgSwitch2.Text = "rgSwitch2";
+            this.rgSwitch2.UseVisualStyleBackColor = true;
+            // 
+            // rgSwitch3
+            // 
+            this.rgSwitch3.Checked = true;
+            this.rgSwitch3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rgSwitch3.DisabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch3.DisabledKnobColor = System.Drawing.Color.Crimson;
+            this.rgSwitch3.EnabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch3.EnabledKnobColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(224)))), ((int)(((byte)(127)))));
+            this.rgSwitch3.IsSolidStyle = false;
+            this.rgSwitch3.Location = new System.Drawing.Point(12, 94);
+            this.rgSwitch3.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch3.Name = "rgSwitch3";
+            this.rgSwitch3.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch3.TabIndex = 2;
+            this.rgSwitch3.Text = "rgSwitch3";
+            this.rgSwitch3.UseVisualStyleBackColor = true;
+            // 
+            // rgSwitch4
+            // 
+            this.rgSwitch4.Checked = true;
+            this.rgSwitch4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rgSwitch4.DisabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch4.DisabledKnobColor = System.Drawing.Color.Crimson;
+            this.rgSwitch4.EnabledBackColor = System.Drawing.Color.Silver;
+            this.rgSwitch4.EnabledKnobColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(224)))), ((int)(((byte)(127)))));
+            this.rgSwitch4.IsSolidStyle = false;
+            this.rgSwitch4.Location = new System.Drawing.Point(12, 120);
+            this.rgSwitch4.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch4.Name = "rgSwitch4";
+            this.rgSwitch4.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch4.TabIndex = 3;
+            this.rgSwitch4.Text = "rgSwitch4";
+            this.rgSwitch4.UseVisualStyleBackColor = true;
+            // 
+            // rgSwitch5
+            // 
+            this.rgSwitch5.Checked = true;
+            this.rgSwitch5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rgSwitch5.DisabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch5.DisabledKnobColor = System.Drawing.Color.Gainsboro;
+            this.rgSwitch5.EnabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch5.EnabledKnobColor = System.Drawing.Color.Black;
+            this.rgSwitch5.Location = new System.Drawing.Point(148, 120);
+            this.rgSwitch5.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch5.Name = "rgSwitch5";
+            this.rgSwitch5.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch5.TabIndex = 7;
+            this.rgSwitch5.Text = "rgSwitch5";
+            this.rgSwitch5.UseVisualStyleBackColor = true;
+            // 
+            // rgSwitch6
+            // 
+            this.rgSwitch6.Checked = true;
+            this.rgSwitch6.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rgSwitch6.DisabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch6.DisabledKnobColor = System.Drawing.Color.Gainsboro;
+            this.rgSwitch6.EnabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch6.EnabledKnobColor = System.Drawing.Color.Black;
+            this.rgSwitch6.Location = new System.Drawing.Point(148, 94);
+            this.rgSwitch6.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch6.Name = "rgSwitch6";
+            this.rgSwitch6.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch6.TabIndex = 6;
+            this.rgSwitch6.Text = "rgSwitch6";
+            this.rgSwitch6.UseVisualStyleBackColor = true;
+            // 
+            // rgSwitch7
+            // 
+            this.rgSwitch7.DisabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch7.DisabledKnobColor = System.Drawing.Color.Gainsboro;
+            this.rgSwitch7.EnabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch7.EnabledKnobColor = System.Drawing.Color.Black;
+            this.rgSwitch7.Location = new System.Drawing.Point(148, 68);
+            this.rgSwitch7.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch7.Name = "rgSwitch7";
+            this.rgSwitch7.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch7.TabIndex = 5;
+            this.rgSwitch7.Text = "rgSwitch7";
+            this.rgSwitch7.UseVisualStyleBackColor = true;
+            // 
+            // rgSwitch8
+            // 
+            this.rgSwitch8.DisabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch8.DisabledKnobColor = System.Drawing.Color.Gainsboro;
+            this.rgSwitch8.EnabledBackColor = System.Drawing.Color.Gray;
+            this.rgSwitch8.EnabledKnobColor = System.Drawing.Color.Black;
+            this.rgSwitch8.Location = new System.Drawing.Point(148, 42);
+            this.rgSwitch8.MinimumSize = new System.Drawing.Size(40, 20);
+            this.rgSwitch8.Name = "rgSwitch8";
+            this.rgSwitch8.Size = new System.Drawing.Size(40, 20);
+            this.rgSwitch8.TabIndex = 4;
+            this.rgSwitch8.Text = "rgSwitch8";
+            this.rgSwitch8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(200, 200);
+            this.Controls.Add(this.rgSwitch5);
+            this.Controls.Add(this.rgSwitch6);
+            this.Controls.Add(this.rgSwitch7);
+            this.Controls.Add(this.rgSwitch8);
+            this.Controls.Add(this.rgSwitch4);
+            this.Controls.Add(this.rgSwitch3);
+            this.Controls.Add(this.rgSwitch2);
+            this.Controls.Add(this.rgSwitch1);
+            this.ForeColor = System.Drawing.Color.Gainsboro;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.RGSwitch rgSwitch1;
+        private Controls.RGSwitch rgSwitch2;
+        private Controls.RGSwitch rgSwitch3;
+        private Controls.RGSwitch rgSwitch4;
+        private Controls.RGSwitch rgSwitch5;
+        private Controls.RGSwitch rgSwitch6;
+        private Controls.RGSwitch rgSwitch7;
+        private Controls.RGSwitch rgSwitch8;
+    }
+}
+
