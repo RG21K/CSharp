c982f9dac5c33f9814e21d39186012848ca1203c
Ricardo Garcia <102972020+RG21K@users.noreply.github.com>
2022-10-11 15:55:11 +0100

    Add files via upload

---
+++ WinForms/Custom Controls/RG RichTextBox/RG RichTextBox/Controls/RGRichTextBox.cs
@@ -0,0 +1,134 @@
+﻿// <copyright file="RGRichTextBox.cs" company="None"> Copyright (c) Ricardo Garcia. </copyright>
+// <author> Ricardo Garcia </author>
+// <date> 2022, 10.10 </date>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RG_RichTextBox.Controls
+{
+    public class RGRichTextBox : RichTextBox
+    {
+        // Last Edited: 2022, 10.10
+0
+        #region <Developer Notes>
+        /*
+         * [Info]
+         * -----------------------------------------------------------------------------------------
+         * 
+         * An Inproved RichTextBox to Suit:
+         * 
+         *      - Chat Windows
+         *      - Logging
+         *      - Etc
+         * 
+         * 
+         * [Features]
+         * -----------------------------------------------------------------------------------------
+         * 
+         *      - Time Stamps
+         *      - Auto Scroll to Caret
+         *      - Customized Message Colors ?
+         * 
+         * [Usage]
+         * -----------------------------------------------------------------------------------------
+         * 
+         * Println(source, message);
+         * 
+         */
+        #endregion
+
+        #region <Constructor>
+        public RGRichTextBox()
+        {
+            BackColor = Color.FromArgb(255, 36, 36, 52);
+            BorderStyle = BorderStyle.None;
+            ReadOnly = true;
+            ForeColor = Color.White;
+        }
+        #endregion
+
+
+        #region <Fields>
+        // -> Stamps
+        private string currentTime = DateTime.Now.ToString("HH:mm");
+        private string timeStamp => $"[{currentTime}]";
+
+        // -> Input Source
+        private string inputSource { get; set; } = string.Empty;
+        private string inputSourceStamp => $"<{inputSource}>";
+        #endregion
+
+
+        #region <Custom Properties>
+        private bool autoScrollToCaret = true;
+        [Category("1. Custom Text"), DisplayName("Auto Scroll to Caret")]
+        [Description("Set AutoScrollToCaret.\n AutoScrolls to Caret when Custom Text is Printed")]
+        [Browsable(true)]
+        public bool AutoScrollToCaret
+        {
+            get { return autoScrollToCaret; }
+            set { autoScrollToCaret = value; }
+        }
+
+        private Color timeStampColor = Color.Gainsboro;
+        [Category("1. Custom Text"), DisplayName("Time Color")]
+        [Description("Set Time Stamp Color")]
+        [Browsable(true)]
+        public Color TimeStampColor
+        {
+            get { return timeStampColor; }
+            set { timeStampColor = value; }
+        }
+
+        private Color inputSourceStampColor = Color.DeepSkyBlue;
+        [Category("1. Custom Text"), DisplayName("InputSource Color")]
+        [Description("Set Input Source Stamp Color")]
+        [Browsable(true)]
+        public Color InputSourceStampColor
+        {
+            get { return inputSourceStampColor; }
+            set { inputSourceStampColor = value; }
+        }
+        #endregion
+
+
+        #region <Methods> : (Public)
+        public void Println(string source, string text) => PrintLine(source, text);
+        #endregion
+
+
+        #region <Methods> : (Private)
+        private void AppendCustomText(string text, Color color)
+        {
+            SelectionStart = TextLength;
+            SelectionLength = 0;
+
+            SelectionColor = color; // ? is this Correct?
+            AppendText(text);
+        }
+
+        private void PrintLine(string textInputSource, string text)
+        {
+            // -> Update Control Data
+            inputSource = textInputSource;
+
+            // -> Append Stamps
+            AppendCustomText($"{timeStamp} ", timeStampColor);
+            AppendCustomText($"{inputSourceStamp} ", inputSourceStampColor);
+
+            // -> Append Text
+            AppendCustomText($"{text}\n", ForeColor);
+
+            // -> Auto Scroll to Caret
+            if (AutoScrollToCaret) { ScrollToCaret(); }
+        }
+        #endregion
+    }
+}
---
+++ WinForms/Custom Controls/RG RichTextBox/RG RichTextBox/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="Form1.Designer.cs" company="None"> Copyright (c) Ricardo Garcia. </copyright>
+// <author> Ricardo Garcia </author>
+// <date> 2022, 10.10 </date>
+
+namespace RG_RichTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rgRichTextBox1 = new RG_RichTextBox.Controls.RGRichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rgRichTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 200);
+            this.panel1.TabIndex = 0;
+            // 
+            // rgRichTextBox1
+            // 
+            this.rgRichTextBox1.AutoScrollToCaret = true;
+            this.rgRichTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(52)))));
+            this.rgRichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rgRichTextBox1.ForeColor = System.Drawing.Color.White;
+            this.rgRichTextBox1.InputSourceStampColor = System.Drawing.Color.DeepSkyBlue;
+            this.rgRichTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.rgRichTextBox1.Name = "rgRichTextBox1";
+            this.rgRichTextBox1.ReadOnly = true;
+            this.rgRichTextBox1.Size = new System.Drawing.Size(176, 176);
+            this.rgRichTextBox1.TabIndex = 0;
+            this.rgRichTextBox1.Text = "";
+            this.rgRichTextBox1.TimeStampColor = System.Drawing.Color.Gainsboro;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(200, 200);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Gainsboro;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Controls.RGRichTextBox rgRichTextBox1;
+    }
+}
+
